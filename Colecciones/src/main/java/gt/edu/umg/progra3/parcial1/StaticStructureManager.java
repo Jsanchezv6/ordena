2db5d7d383a7681b8ed6ca7d456e17d54561da01
Jsanchezv6 <85458422+Jsanchezv6@users.noreply.github.com>
2023-03-11 10:35:28 -0600

    commit
    primero

---
+++ Colecciones/src/main/java/gt/edu/umg/progra3/parcial1/StaticStructureManager.java
@@ -0,0 +1,23 @@
+package gt.edu.umg.progra3.parcial1;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+public class StaticStructureManager<T extends Comparable<T>> {
+
+    private T[] values;
+
+    public StaticStructureManager(T[] values) {
+        this.values = values;
+    }
+
+    public T[] ordenar() {
+        Arrays.sort(values);
+        return values;
+    }
+
+    public T[] ordenarInversa() {
+        Arrays.sort(values, Collections.reverseOrder());
+        return values;
+    }
+}
